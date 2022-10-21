54dd771845e1a88995743a58c22fe061000969f3
AzizAbd <91793452+AzizAbd@users.noreply.github.com>
2022-10-21 18:19:57 +0200

    first commit

---
+++ Aziz.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Aziz {
+    public static void main(String[] args) {
+        Scanner chaine = new Scanner(System.in);
+        System.out.println("entrer nom et prénom");
+        String chaine1 = chaine.nextLine();
+        System.out.println("Bonjour " + chaine1 + "Vous etes le bienevenu dans notre project java");
+    }
+}
